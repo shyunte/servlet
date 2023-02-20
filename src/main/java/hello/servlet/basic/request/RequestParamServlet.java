d688a3972e892a47e5f16207a24e9e3f568c25b0
hyun12 <sht4441@gmail.com>
2023-02-20 16:53:24 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 파라미터 전송
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+
+@WebServlet(name ="requestParamServlet", urlPatterns = "/reqeust-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println("RequestParamServlet.service");
+//        System.out.println("[전체 파라미터 조회] -start");
+//        request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+//        System.out.println("[전체 파라미터 조회] -end");
+//        Enumeration<String> parameterNames = request.getParameterNames();
+    }
+}
